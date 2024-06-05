1464d6b9d77f5bdd74a2ae00578d8c5b4084945f
An Pahn <128393021+AnDepChai@users.noreply.github.com>
2024-06-05 16:01:00 +0700

    Add files via upload

---
+++ responses.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ownerResponses: [
+    'Dạ! có em đây :3',
+    'Em nghe đây, anh An Pahn!',
+    'Có việc gì cần em không anh An Pahn?'
+  ],
+  otherResponses: [
+    'Bạn là ai!, tôi có quen bạn à tôi chỉ có mỗi anh An Pahn thôi ❤️️',
+    'Xin lỗi, tôi chỉ nghe lệnh từ anh An Pahn thôi.',
+    'Tôi không biết bạn là ai, xin đừng làm phiền.'
+  ]
+};
