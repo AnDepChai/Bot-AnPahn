f5c8366a7a80638f85feca8d50a0339c92a2704d
An Pahn <128393021+AnDepChai@users.noreply.github.com>
2024-06-29 19:23:05 +0700

    Delete trangthaibot.js

--- trangthaibot.js
+++
@@ -1,9 +0,0 @@
-function setBotStatus(client) {
-  
-    client.on("ready", () => {
-  client.user.setStatus("dnd");
-  client.user.setActivity("với An Pahn! | /Help", { Type: 'PLAYING'});
- });
-}
-
-module.exports = setBotStatus;
